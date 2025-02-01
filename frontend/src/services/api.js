21ded800e08af7d49ec12e8c6c3aa9be84575fe4
Aymen Elouaer <103264853+AymenElouaer@users.noreply.github.com>
2025-02-01 22:19:07 +0100

    Update api.js to include the deployed backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api"; // backend URL
+const API_URL = "https://url-shortener-backend-gamma.vercel.app"; // backend URL
 
 export const shortenUrl = async (longUrl, customAlias, expiration) => {
   const response = await axios.post(`${API_URL}/shorten`, {
