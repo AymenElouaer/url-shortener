7c726030d31fca8d804a0134ee8ef03a556eff9c
Aymen Elouaer <103264853+AymenElouaer@users.noreply.github.com>
2025-02-02 01:24:31 +0100

    changed backend url

--- backend/app.js
+++ backend/app.js
@@ -7,22 +7,14 @@ require("dotenv").config();
 
 const app = express();
 
-// Enable CORS for all origins (for testing)
-app.use(
-  cors({
-    origin: "*", // Allow all origins (replace with your frontend URL in production)
-    methods: ["GET", "POST"],
-    credentials: true,
-  })
-);
-
+const corsOptions = {
+  origin: "https://url-shortener-eight-beta.vercel.app", // Frontend URL
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"],
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(errorHandler);
-
-// Test Route
-app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello, the backend is working!" });
-});
 
 // Routes
 app.use("/api", routes); // Use the routes defined in routes/index.js
