a5ca75817503235c9c420f74c8eeccd00884c4c5
Aymen Elouaer <103264853+AymenElouaer@users.noreply.github.com>
2025-02-02 01:52:48 +0100

    fixed frontend issue

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "url-shortener-backend-20d1.onrender.com/api"; // backend URL
+const API_URL = "url-shortener-backend-20d1.onrender.com"; // backend URL
 
 export const shortenUrl = async (longUrl, customAlias, expiration) => {
   const response = await axios.post(`${API_URL}/api/shorten`, {
